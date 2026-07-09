52f2d6b1cded71b8a925585e2b5125ba6a7ffa70
aline28aguilar <aline.28aguilar@gmail.com>
2026-07-08 22:34:14 -0600

    Organizacion de archivos del proyecto :wq

--- srvdevuelve/api/devuelve.php
+++ srvdevuelve/api/devuelve.php
@@ -3,6 +3,6 @@
 require_once __DIR__ . "/../libservidorphp/devuelveJson.php";
 
 devuelveJson([
- "nombre" => "pp",
- "mensaje" => "Hola Aline Lizeth Aguilar Alcanara",
+ "nombre" => "Aline Lizeth Aguilar Alcanatara",
+ "mensaje" => "Hola ",
 ]);
